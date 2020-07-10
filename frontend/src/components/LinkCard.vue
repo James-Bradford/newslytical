ef8808215bf0fabef9296fdac1fe627f1e6f565d
James Bradford <j.bradford2@ncl.ac.uk>
2020-07-10 11:01:41 +0100

    Added PSL for hostname extraction

--- frontend/src/components/LinkCard.vue
+++ frontend/src/components/LinkCard.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
- //     v-expansion-panels(popout)
-        v-expansion-panel(v-for='(url, u) in this.tweet.entities.urls' :key='u')
-          v-expansion-panel-header {{url.expanded_url}}
+      v-expansion-panels(popout)
+        v-expansion-panel(v-for='(url, u) in this.whois' :key='u')
+          v-expansion-panel-header {{url.WhoisRecord.domainName}}
             v-expansion-panel-content
 </template>
 
@@ -15,21 +15,28 @@ export default {
     urls: [],
     whois: []
   },
+  computed: {
+    tweet() {
+      return this.$store.state.tweet;
+    }
+  },
   created() {
+    console.log(this.tweet.entities.urls);
     var urls = this.tweet.entities.urls;
     var whois = [];
     var self = this;
+
 
     for (let i = 1; i < urls.length; i++) {
       Api.get(`whois/${urls[i]}`)
         .then(function(result) {
           whois.push(result.data);
-          console.log(result.data);
         })
         .catch(error => {
           throw new Error("API ERROR");
         });
         whois = this.whois;
+        console.log(whois);
     }
   },
 };
