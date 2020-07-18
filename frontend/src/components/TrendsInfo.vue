cde689d17834f074e142718dfbe14d61a2b062f9
James Bradford <j.bradford2@ncl.ac.uk>
2020-07-18 14:15:08 +0100

    Changed keyword extraction package

--- frontend/src/components/TrendsInfo.vue
+++ frontend/src/components/TrendsInfo.vue
@@ -24,24 +24,7 @@ export default {
     }
   },
   created() {
-    var commonWords = [];
-    var tweetWords = unique(this.tweetText.toLowerCase());
 
-    console.log(tweetWords);
-
-    for (let i = 0; i < this.trends.length; i++) {
-        var trend = this.trends[i].name.toLowerCase().replace(/[^a-zA-Z ]/g, "");
-
-            if (tweetWords.includes(trend) || tweetWords.includes("#" + trend)) {
-                commonWords.push({
-                    'name': trend,
-                    'tweet_volume': this.trends[i].tweet_volume
-                });
-            }
-        
-    }
-    this.commonWords = commonWords;
-    console.log(this.commonWords);
   }
 };
 </script>
