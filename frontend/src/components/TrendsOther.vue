fbaff5bc553af3cbb3ff832d1985e87dac056676
James Bradford <j.bradford2@ncl.ac.uk>
2020-07-23 10:40:28 +0100

    UI improvements

--- frontend/src/components/TrendsOther.vue
+++ frontend/src/components/TrendsOther.vue
@@ -1,9 +1,12 @@
 <template lang="pug">
 
-    v-chip-group(multiple='' active-class='primary--text')
+  
+    v-chip-group(multiple='' active-class='primary--text' v-if="relatedTopics.length > 0")
       v-chip(v-for='(trend, u) in this.relatedTopics' :key='u' target='_blank' :href='`https://fullfact.org/search/?q=${trend.topic.title}`' color='white')
         | {{ trend.topic.title }}
-            
+
+    div(v-else)
+      span.font-weight-light.white--text.pa-5 No other topics found for this keyword.  Use the quick actions above for more context.     
         
 </template>
 
