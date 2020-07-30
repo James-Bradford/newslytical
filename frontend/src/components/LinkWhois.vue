9a6e3d00db84a1b97166ad99135a4d7aae385a3f
James Bradford <j.bradford2@ncl.ac.uk>
2020-07-30 20:23:16 +0100

    UI Improvements

--- frontend/src/components/LinkWhois.vue
+++ frontend/src/components/LinkWhois.vue
@@ -6,16 +6,16 @@
         | Links
       v-card-text
         v-divider.ma-2(color="white")
-      v-expansion-panels(v-if="this.tweet.entities.urls.length > 0")
+      v-expansion-panels(v-if="this.tweet.entities.urls.length > 0" v-model="defaultPanel")
         v-expansion-panel.rounded-0(v-for='(url, u) in this.tweet.entities.urls' :key="u")
           v-expansion-panel-header {{ whois[u].WhoisRecord.domainName }}
           v-expansion-panel-content 
               div(v-if='isHTTPS(url.expanded_url)') 
                 v-icon.mdi.mdi-lock-check(color="success")
-                | This website uses HTTPS encryption
+                | HTTPS encryption
               div(v-else) 
                 v-icon.mdi.mdi-lock-open-alert(color="warning")
-                | This website does not use HTTPS encryption
+                | No HTTPS encryption
               div(v-if='whois[u].WhoisRecord.registrant !== undefined') 
                 v-icon.mdi.mdi-earth
                 | Registered in: {{ whois[u].WhoisRecord.registrant.country }}
@@ -24,10 +24,10 @@
                 | Registrant: {{ whois[u].WhoisRecord.registrant.organization }}
               div(v-else)
                 v-icon.mdi.mdi-earth-off
-                | No location found for this domain
+                | No location found
                 br
                 v-icon.mdi.mdi-account-off
-                | No registrant information for this domain
+                | No registrant information
               div(v-if='whois[u].WhoisRecord.registryData.createdDate !== undefined')
                 v-icon.mdi.mdi-clock
                 | Created: {{ whois[u].WhoisRecord.registryData.createdDate }}
@@ -37,23 +37,49 @@
 </template>
 
 <script>
+//Import dependencies
 import axios from "axios";
 import Api from "../services/Api";
 
+/**
+ * Provides a domain lookup
+ */
 export default {
   name: "LinkWhois",
   props: {
+    /**
+     * Urls to be investigated
+     */
     urls: [],
   },
+  data() {
+    return {
+      /**
+       * Default panel open
+       */
+      defaultPanel: 0
+    }
+  },
   computed: {
+    /**
+     * Tweet object
+     */
     tweet() {
       return this.$store.state.tweet;
     },
+    /**
+     * Whois lookup object
+     */
     whois() {
       return this.$store.state.whois;
     }
   },
   methods: {
+    /**
+     * Returns true if the URL uses HTTPS
+     * 
+     * @param url URL
+     */
     isHTTPS(url) {
       if (url.match(/^[^:]+/) == "https") {
         return true
